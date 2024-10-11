27f87bad0b29ca833c6093098c5c1ffbb0a136b4
wudongge <dg.wu@liaoke.tv>
2024-10-11 14:23:08 +0800

    添加 presentedViewController == nil 判断

--- TZImagePickerController/Classes/TZPhotoPickerController.m
+++ TZImagePickerController/Classes/TZPhotoPickerController.m
@@ -891,7 +891,9 @@ static CGFloat itemMargin = 5;
         if (tzImagePickerVc.uiImagePickerControllerSettingBlock) {
             tzImagePickerVc.uiImagePickerControllerSettingBlock(_imagePickerVc);
         }
-        [self presentViewController:_imagePickerVc animated:YES completion:nil];
+        if (self.presentedViewController == nil) {
+            [self presentViewController:_imagePickerVc animated:YES completion:nil];
+        }
     } else {
         NSLog(@"模拟器中无法打开照相机,请在真机中使用");
     }
